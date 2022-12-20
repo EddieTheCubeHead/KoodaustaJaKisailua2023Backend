7fb4db93742cf8596b5199444088caff7ccc3618
EddieTheCubeHead <eetu.asikainen1204@gmail.com>
2022-12-20 19:58:12 +0200

    Refactor project a little. Add models.py

---
+++ src/models.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+from dataclasses import dataclass
+
+
+@dataclass
+class EvolutionChain:
+    name: str
+    pokedex_number: int
+    sprite_link: str
+    evolution_condition: str
+    evolves_to: list[EvolutionChain]
+
+
+@dataclass
+class Pokemon:  # route: /pokemon/{name}
+    name: str
+    pokedex_number: int
+    artwork_link: str
+    types: list[str]
+    abilities: list[str]
+    evolution_chain: EvolutionChain
+
+
+@dataclass
+class ListPokemon:  # route: /pokemons
+    name: str
+    pokedex_number: int
+    sprite_link: str
+    types: list[str]
+
+
+@dataclass
+class Type:  # route: /type/{name}
+    name: str
+    id: int
+    offensive_multipliers: {str: float}
+    defensive_multipliers: {str: float}
+
+
+@dataclass
+class TypeMatrix:  # route: /types
+    types: list[list[str|float]]
+
+
+@dataclass
+class ExpGainPokemonData:
+    name: str
+    level: int
+
+
+@dataclass
+class ExpGainParams:
+    attacker: ExpGainPokemonData
+    defender: ExpGainPokemonData
+
+
+@dataclass
+class ExpGain:  # route: /exp_gain, params: ExpGainParams
+    exp_gained: int
