a77cc17538ad14628d6148c496b49472ffaa8ea0
EddieTheCubeHead <eetu.asikainen@student.lut.fi>
2022-12-27 10:22:36 +0200

    Expand test cases to cover all basic pokemon fetch cases

--- features/environment.py
+++ features/environment.py
@@ -1,3 +1,4 @@
+import requests
 from behave.runner import Context
 from starlette.testclient import TestClient
 
@@ -7,3 +8,5 @@ from src.routes import app
 def before_all(context: Context):
     context.client = TestClient(app)
     context.response = None
+    all_pokemon = requests.get("https://pokeapi.co/api/v2/pokemon?limit=9999").json()["results"]
+    context.all_pokemon = [(pokemon["name"], int(pokemon["url"].split("/")[-2])) for pokemon in all_pokemon]
--- features/steps/steps.py
+++ features/steps/steps.py
@@ -1,3 +1,5 @@
+import random
+
 from behave import *
 from behave.runner import Context
 
@@ -30,9 +32,30 @@ def parse_to_dict(table):
 def step_impl(context: Context):
     expected_model = parse_to_dict(context.table)
     actual_model = context.response.json()
+    assert_valid_model(actual_model, expected_model)
+
+
+def assert_valid_model(actual_model, expected_model):
     for expected_key, expected_value in expected_model.items():
         assert expected_key in actual_model, \
             f"Expected to find the field '{expected_key}' in the model {actual_model}"
         assert actual_model[expected_key] == expected_value, \
             f"Expected the field '{expected_key}' to equal '{expected_value}' in the model {actual_model}, but was " \
             f"'{actual_model[expected_key]}'"
+
+
+@when("fetching a random pokemon from /pokemon/name")
+def step_impl(context: Context):
+    context.fetched_name, context.fetched_number = random.choice(context.all_pokemon)
+    get(context, f"/pokemon/{context.fetched_name}")
+
+
+@then("pokemon name, pokedex number and artwork link returned")
+def step_impl(context: Context):
+    artwork_link = f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" \
+                   f"{context.fetched_number}.png"
+    expected_model = {"name": context.fetched_name,
+                      "pokedex_number": context.fetched_number,
+                      "artwork_link": artwork_link}
+    actual_model = context.response.json()
+    assert_valid_model(actual_model, expected_model)
