beac997baae0b57ed3603989943d94c45e46fc68
EddieTheCubeHead <eetu.asikainen1204@gmail.com>
2023-01-15 22:28:39 +0200

    Add cache decorator

--- src/pokeapi_client.py
+++ src/pokeapi_client.py
@@ -1,7 +1,10 @@
+from functools import lru_cache
+
 from fastapi import HTTPException
 import requests
 
 
+@lru_cache
 def get(url: str):
     request = requests.get(url)
     if request.status_code != 200:
